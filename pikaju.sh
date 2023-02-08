e41a250bb483f58b067a3dd224207a769a1b59b9
click23 <55525839+click23@users.noreply.github.com>
2023-02-08 10:13:28 +0330

    Update pikaju.sh

--- pikaju.sh
+++ pikaju.sh
@@ -1,8 +1,22 @@
-apt update && apt upgrade -y
+#!/bin/bash
+# Ask the user for their name
+echo Hello, Please enter the domain to be installed on!
+read domain
+echo Now enter an email to be used for acme shell script!
+read email
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt clean -y
+
 apt install curl socat -y
+
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m enschu@gmail.com
-~/.acme.sh/acme.sh --issue -d v157173.syntaxdump.com --standalone --force
-~/.acme.sh/acme.sh --installcert -d v157173.syntaxdump.com --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone --force
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
+
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
