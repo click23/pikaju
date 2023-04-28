a305af04d8107a2e08324ceb3ef31182da7ea3f2
click23 <55525839+click23@users.noreply.github.com>
2023-04-28 12:14:58 +0330

    Update pikaju.sh

--- pikaju.sh
+++ pikaju.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-# Ask the user for their name
-echo Hello, Please enter the domain to be installed on!
-read -r Domain
-echo Now enter an email to be used for acme shell script!
-read -r Email
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-sudo apt clean -y
+# Ask the user for the domain and email
+read -p "Enter the domain to be installed on: " DOMAIN
+read -p "Enter an email to be used for the ACME shell script: " EMAIL
 
-apt install curl socat -y
+# Update the system and install required packages
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+sudo apt install curl socat -y
 
+# Install and configure ACME.sh to obtain an SSL certificate
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m "$Email"
-~/.acme.sh/acme.sh --issue -d "$Domain" --standalone --force
-~/.acme.sh/acme.sh --installcert -d "$Domain" --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh --register-account -m "$EMAIL"
+~/.acme.sh/acme.sh --issue -d "$DOMAIN" --standalone --force
+~/.acme.sh/acme.sh --installcert -d "$DOMAIN" --key-file /root/private.key --fullchain-file /root/cert.crt
 
+# Install X-UI panel
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
